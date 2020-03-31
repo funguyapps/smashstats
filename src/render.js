eff43c2d15976ab74af502332164aa912f499086
Nick <trashemail430@gmail.com>
2020-03-30 20:14:27 -0500

    Initial commit.

---
+++ src/render.js
@@ -0,0 +1,28 @@
+//import Database from "./data.js";
+const sqlite = require("sqlite3").verbose();
+
+class Database
+{
+    static path = "";
+
+    static open()
+    {
+
+    }
+
+    static close()
+    {
+
+    }
+
+    static getTop3()
+    {
+        this.open();
+
+        // stuff
+
+        this.close();
+
+        return ""; // results
+    }
+}
