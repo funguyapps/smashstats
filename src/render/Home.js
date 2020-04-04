10440312d73e9b89bd1c5a1efa8132b91462b0f0
pugnaciousplatypus <62917714+pugnaciousplatypus@users.noreply.github.com>
2020-04-03 21:40:05 -0500

    Added all fighter pictures & hooked up top 3 images.

--- src/render/Home.js
+++ src/render/Home.js
@@ -13,6 +13,10 @@ const box2Right = document.getElementById("box2Right");
 const box3Left = document.getElementById("box3Left");
 const box3Right = document.getElementById("box3Right");
 
+const box1Img = document.getElementById("box1Img");
+const box2Img = document.getElementById("box2Img");
+const box3Img = document.getElementById("box3Img");
+
 // data access functions
 function getTop3(sortMethod)
 {
@@ -27,16 +31,28 @@ function getTop3(sortMethod)
         db.close();
              
         // box 1
-        box1Left.textContent = rows[0].name;
+        const box1Name = rows[0].name;
+
+        box1Left.textContent = box1Name;
         box1Right.textContent = rows[0].sort;
 
+        box1Img.src = path.join(__dirname, "../assets/fighters", box1Name + ".png");
+
         // box 2
-        box2Left.textContent = rows[1].name;
+        const box2Name = rows[1].name;
+
+        box2Left.textContent = box2Name;
         box2Right.textContent = rows[1].sort;
 
+        box2Img.src = path.join(__dirname, "../assets/fighters", box2Name + ".png");
+
         // box 3
+        const box3Name = rows[2].name;
+        
         box3Left.textContent = rows[2].name;
         box3Right.textContent = rows[2].sort;
+
+        box3Img.src = path.join(__dirname, "../assets/fighters", box3Name + ".png");
     });
 }
 
