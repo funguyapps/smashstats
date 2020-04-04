4d457b23063461c0a4a977eaca73efe85d26b115
pugnaciousplatypus <62917714+pugnaciousplatypus@users.noreply.github.com>
2020-04-03 21:16:03 -0500

    Hooked up top 3 results text to content boxes.

--- src/render/Home.js
+++ src/render/Home.js
@@ -1,25 +1,44 @@
+// imports
 const sqlite = require("sqlite3").verbose();
+const path = require("path");
 
-const path = __dirname + "/../stats.db";
+// global variables
+const dbPath = path.join(__dirname, "../stats.db");
 
-async function getTop3(sortMethod)
+// doc references
+const box1Left = document.getElementById("box1Left");
+const box1Right = document.getElementById("box1Right");
+const box2Left = document.getElementById("box2Left");
+const box2Right = document.getElementById("box2Right");
+const box3Left = document.getElementById("box3Left");
+const box3Right = document.getElementById("box3Right");
+
+// data access functions
+function getTop3(sortMethod)
 {
-    const db = new sqlite.Database(path);
+    const db = new sqlite.Database(dbPath);
     
-    const sql = "SELECT Name name FROM Fighters ORDER BY ? DESC";
+    const sql = "SELECT Name name, " + sortMethod + " sort FROM Fighters ORDER BY " + sortMethod + " DESC";
 
-    await db.all(sql, [sortMethod], (err, rows) => 
+    db.all(sql, (err, rows) => 
     {
-        if (err) {console.log(err);}
-
-        console.log(rows);
+        if (err) { console.log(err); }
 
         db.close();
-    
-        return rows;
+             
+        // box 1
+        box1Left.textContent = rows[0].name;
+        box1Right.textContent = rows[0].sort;
+
+        // box 2
+        box2Left.textContent = rows[1].name;
+        box2Right.textContent = rows[1].sort;
+
+        // box 3
+        box3Left.textContent = rows[2].name;
+        box3Right.textContent = rows[2].sort;
     });
 }
 
-let results = getTop3("Score");
-
-console.log(results);
+// main logic
+getTop3("Score");
